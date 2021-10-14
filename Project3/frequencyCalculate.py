347c9c8648b34b08dde37af665daeccceef02676
Zane Rickert <zane.rickert@gmail.com>
2021-10-13 21:05:24 -0400

    update

--- Project3/frequencyCalculate.py
+++ Project3/frequencyCalculate.py
@@ -1,36 +1,52 @@
+# Project No.: 3
+# Author: Zane Rickert
+# Description: This program will ask the user how many strings of text they 
+#              want to enter, it will then compress the string into just
+#              letters, and print the amount of times each letter shows up
+#              in their passage of text. It will also check if the text is 
+#              a palindrone in an external function. 
+import testModule
 def main():
     numberList = [0] * 26 
     lettersList=[(chr(ord('a')+i)) for i in range(26)]
     
     while True:
         try:
-            repeatAmount = int(input("Enter a positve integer: "))
+            m = int(input("Enter the number of strings you want to read: "))
         except ValueError:
             continue
-        if repeatAmount <= 0:
+        if m <= 0:
             continue
         else:
             break
         
-    combinedString = userString(repeatAmount) 
+    print(m)
+
+    combinedString = userString(m) 
     showString(combinedString) 
     
     lowerCaseString = convertStringLower(combinedString)
 
     stringList = stringToList(lowerCaseString)
     
-    compressedString = compressList(stringList)
-    frequencyCount(numberList, lettersList, compressedString)
-    print(numberList)
+    compressedList = compressList(stringList)
+    frequencyCount(numberList, lettersList, compressedList)
+
+    palindrome = testModule.checkPalindrome(compressedList)
+    if palindrome:
+        print("These letters do form a palindrome.")
+    else:
+        print("These letters do not form a palindrome.")
+    print('')
+    showFrequency(numberList, lettersList)
 
 def userString(iterator):
-    combinedString = ''
-    print("Input {} strings.".format(iterator))
+    st = ''
     for i in range(iterator):
-        strInput = input("Enter a string: ")
-        combinedString += strInput
+        strInput = input(f"Enter string {i + 1}: ")
+        st += strInput
 
-    return combinedString
+    return st
 
 def showString(string):
     print(string)
@@ -43,24 +59,26 @@ def stringToList(string):
     for char in string:
         strList.append(char)
 
-    print(strList)
+    print(''.join(strList))
     return strList
 
-def compressList(listItem):
-    stringConvert = ''
-    for item in listItem:
-        if 'a' <= item  <= 'z':
-            stringConvert += item
+def compressList(originalList):
+    compressedList = [] 
+    for letter in originalList:
+        if 'a' <= letter  <= 'z':
+            compressedList.append(letter) 
 
-    return stringConvert
+    print(''.join(compressedList))
+    return compressedList
 
 def frequencyCount(numberArray, lettersArray, string):
     for char in string:
         numberArray[ord(char) - 97] += 1
 
-def showFrequency():
-    pass
-
+def showFrequency(freqDistributionList, lettersArray):
+    print("Folliwing is the frequency distribution for the letters in the list.")
+    for i, letter in enumerate(lettersArray):
+        print(letter, "*" * freqDistributionList[i])
 
 if __name__ == '__main__':
     main()
---
+++ Project3/testModule.py
@@ -0,0 +1,5 @@
+def checkPalindrome(ls):
+   if ls == ls[::-1]:
+       return True
+   else:
+       return False 

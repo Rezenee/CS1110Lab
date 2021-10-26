c007fa00969d2df6733ac8224d36a38fc28da5d1
Zane Rickert <zane.rickert@gmail.com>
2021-10-26 14:52:08 -0400

    working

--- project4/project4.py
+++ project4/project4.py
@@ -1,13 +1,21 @@
-def main():
-    filename_prefix = input("Enter a file prefix: ")
+# Project No.: Project 4
+# Author: Zane Rickert
+# This program lets a user edit an existing file that has entries of users 
+# in a bank. They can withdrawal, deposit, or close accounts. The updated
+# information is put in a new output file. 
 
+def main():
+    # Gets filename prefix, and assigns txt of old and new file
+    filename_prefix = input("Enter a file prefix: ")
     filename_old = filename_prefix + '_old.txt'
     filename_new = filename_prefix + '_new.txt'
 
+    # Attempts to open both files
     try:
         with open(filename_old, 'r') as f_old, open(filename_new, 'w') as f_new:
             while True:
-                acount_closed = False
+                account_closed = False
+                # Gets each line from the original file one at a time to work with
                 line = f_old.readline().strip()
 
                 # Gets the id number, and returns if it is EOF
@@ -18,116 +26,102 @@ def main():
                     return
 
                 # Get the balance and name if line is valid entry
-                balance = get_balance(line)
+                balance_dollars = get_balance(line) 
+                balance_cents = int(balance_dollars * 100) 
                 name = get_name(line)
-                print(f"Verifying input: {number} {balance} {name}")
+                print(f"Verifying input: {number} {balance_dollars} {name}")
 
-                # Goes through all possible actions the user wants for each account
+                # Goes through all possible actions the user wants for account
+                # When user types a, break out of loop to read next user
                 while True:
                     command = get_command()
                     if command == 'a':
+                        balance_dollars = balance_cents / 100
+                        print(f"New balance: {number} {balance_dollars} {name}")
                         break
 
-                    if command == 'c':
-                        acount_closed = close_account(balance)
-                        if acount_closed:
-                            break
+                    elif command == 'c':
+                        account_closed = close_account(balance_cents)
 
-                    if command == 'd':
-                        balance = deposit(balance)
-                    if command == 'w':
-                        balance = withdrawal(balance)
+                    elif command == 'd':
+                        balance_cents = deposit(balance_cents)
+
+                    else:
+                        balance_cents = withdrawal(balance_cents)
 
                 # if the person did not say to close the acount, write the new values
-                if not acount_closed:
-                    f_new.write(f'{number} {balance:10.2f} {name}\n')
+                if not account_closed:
+                    f_new.write(f'{number} {balance_dollars:10.2f} {name}\n')
 
-    except FileNotFoundError:
+    # If either file fails to open quit the program.
+    except IOError:
         print(f"The file name {filename_old} does not exist")
         return
 
 def close_account(balance):
+    # Checks if the account is empty before closing
     if balance > 0:
         print("Account not closed because money is still in it.")
         return False
+
+    print("Account is closed")
     return True 
 
 def deposit(balance):
     while True:
+        # asks user for valid deposit amount, repeats until valid amount submitted
         try:
-            deposit_amount = float(input("Enter a deposit amount: "))
+            deposit_amount = int(float(input("Enter a deposit amount: ")) * 100)
         except ValueError:
             print("Invaled Value")
             continue
         if deposit_amount < 0:
             print("You cannot deposit a negative amount.")
             continue
-        if deposit_amount + balance > 9999999.99:
+        if deposit_amount + balance > 999_999_999:
             print("You cannot have more than 9999999.99 in your account.")    
             continue
+
         return balance + deposit_amount
 
 def withdrawal(balance):
     while True:
+        # asks user for valid withdrawal amount, repeats until valid amount is submitted
         try:
-            withdrawal_amount = float(input("Enter withdrawal amount: "))
+            withdrawal_amount = int(float(input("Enter withdrawal amount: ")) * 100)
         except ValueError:
             print("Invalid Value")
             continue
         if withdrawal_amount < 0:
             print("You cannot withdrawal a negative number.")
+            continue
         if withdrawal_amount > balance:
             print("You cannot afford this withdrawal")
             continue
-        print(f"New balance = {balance - withdrawal_amount}")
+
         return balance - withdrawal_amount
         
 def get_number(line):
+    # Gets the first 6 characters of line, this is always the ID
     return int(line[0:6])
 
 def get_balance(line):
-    return int(float(line[6:17]) * 100)
+    # Digits index 6 through 17 are the balance
+    return float(line[6:17])
 
 def get_name(line):
+    # The rest of the digits are the name
     return line[18::] 
 
 def get_command():
+    # Re propmt the user until the command is a valid command
     while True:
         command = input("Enter a command (a,c,d,w): ")
         if command.lower() not in ['a','c','d','w']:
             print(f"Entered an invalid command {command}")
             continue
+
         return command.lower()
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     main()
